fd203a61dd043d7c292ed716424c0a0f790a86a5
Leonardo Servan <leonardoservan@gmail.com>
2025-02-19 12:01:59 -0300

    Modifica o script que calcula variação para todos países.
    Agora o script calcula a variação para todos os países,
    enquanto antes ele calculava a variação apenas para todos
    os países que começam com a letra "a".

--- code/variacao_temperatura_todos.sh
+++ code/variacao_temperatura_todos.sh
@@ -1,6 +1,6 @@
 # Calcula a variação de temperatura para todos os países
 echo variacao_C_por_ano,pais
-for arquivo in dados/temperatura/a*.csv
+for arquivo in dados/temperatura/*.csv
 do
 	python code/variacao_temperatura.py $arquivo
 done
